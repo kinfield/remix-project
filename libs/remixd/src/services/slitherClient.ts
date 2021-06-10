49b6757e906bafd491a8b0523ed0803dc3a65738
aniket-engg <aniketengg.225@gmail.com>
2021-06-10 19:58:39 +0530

    send ast to save

--- libs/remixd/src/services/slitherClient.ts
+++ libs/remixd/src/services/slitherClient.ts
@@ -1,6 +1,8 @@
 import * as WS from 'ws' // eslint-disable-line
+import { writeFileSync, readFileSync} from 'fs'
 import { PluginClient } from '@remixproject/plugin'
-const { spawn } = require('child_process')
+import { absolutePath } from '../utils'
+const { spawn, spawnSync, execSync } = require('child_process')
 
 export class SlitherClient extends PluginClient {
   methods: Array<string>
@@ -20,30 +22,48 @@ export class SlitherClient extends PluginClient {
     this.currentSharedFolder = currentSharedFolder
   }
 
-  analyse (filePath: string) {
+  // transform (slitherResults) {
+    // Analysis Description
+// Analysis Short title
+// Confidence
+// Severity
+// Source Map
+// Module Category
+// Reference link
+// Example code snippets
+// Other data
+  // }
+
+  analyse (ast: string) {
+    console.log('ast received----->', ast)
     return new Promise((resolve, reject) => {
       if (this.readOnly) {
         const errMsg = '[Slither Analysis]: Cannot analyse in read-only mode'
         return reject(new Error(errMsg))
       }
-      const outputFile = 'remix-slitherReport_' + Date.now() + '.json' 
-      const cmd = `slither ${filePath} --json ${outputFile}`
-      const options = { cwd: this.currentSharedFolder, shell: true }
-      const child = spawn(cmd, options)
-      let result = ''
-      let error = ''
-      child.stdout.on('data', (data) => {
-        const msg = `[Slither Analysis]: ${data.toString()}`
-        console.log('\x1b[32m%s\x1b[0m', msg)
-        result += msg + '\n'
-      })
-      child.stderr.on('data', (err) => {
-        error += `[Slither Analysis]: ${err.toString()}`
-      })
-      child.on('close', () => {
-        if (error) reject(error)
-        else resolve(result)
-      })
+      const result = spawnSync('ls', { cwd: this.currentSharedFolder })
+      const dirContent = result.stdout.toString()
+      if(!dirContent.includes('temp_remix')) execSync('mkdir temp_remix', { cwd: this.currentSharedFolder })
+      const astFilePath = absolutePath('./', this.currentSharedFolder) + '/temp_remix/' + 'ast.json'
+      writeFileSync(astFilePath, JSON.stringify(ast, null, '\t'))
+      // const outputFile = 'temp_remix/slitherReport_' + Date.now() + '.json' 
+      // const cmd = `slither --solc-ast ${astFilePath} --json ${outputFile}`
+      // const options = { cwd: this.currentSharedFolder, shell: true }
+      // const child = spawn(cmd, options)
+      // let result = ''
+      // let error = ''
+      // child.stdout.on('data', (data) => {
+      //   const msg = `[Slither Analysis]: ${data.toString()}`
+      //   console.log('\x1b[32m%s\x1b[0m', msg)
+      //   result += msg + '\n'
+      // })
+      // child.stderr.on('data', (err) => {
+      //   error += `[Slither Analysis]: ${err.toString()}`
+      // })
+      // child.on('close', () => {
+      //   if (error) reject(error)
+      //   else resolve(result)
+      // })
     })
   }
 }
